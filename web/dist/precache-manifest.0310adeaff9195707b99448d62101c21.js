3951ba361693057a1dcb46d2f915eb135b26e53b
blane thompson <blane1245@hotmail.co.uk>
2020-11-14 23:46:18 +0000

    Add dist to repository

---
+++ web/dist/precache-manifest.0310adeaff9195707b99448d62101c21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8862e42f1a29732d906",
+    "url": "/css/app.d569caf2.css"
+  },
+  {
+    "revision": "c3afb155df656ef1e996",
+    "url": "/css/chunk-vendors.ec8ed08e.css"
+  },
+  {
+    "revision": "e748d74826c0dcedd3a2224f83306536",
+    "url": "/img/banner.jpg"
+  },
+  {
+    "revision": "8a4ba0ea9b1e5772d7163aba42bcc437",
+    "url": "/img/brand.8a4ba0ea.svg"
+  },
+  {
+    "revision": "d6169d912fdb96855a734dfcf728ee5a",
+    "url": "/img/default.png"
+  },
+  {
+    "revision": "f02a6b0a262b27b93c1156ef0746490a",
+    "url": "/img/integration.png"
+  },
+  {
+    "revision": "d9002dbc6a63217e58cac34ca5e443de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8862e42f1a29732d906",
+    "url": "/js/app.164d5157.js"
+  },
+  {
+    "revision": "c3afb155df656ef1e996",
+    "url": "/js/chunk-vendors.290b0833.js"
+  },
+  {
+    "revision": "6e072850c614ec08708283370da60f85",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ web/dist/precache-manifest.7d4eeb88183dc6f0aa18caa837f76aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00f04b9b31b06ef272a",
+    "url": "/css/app.d569caf2.css"
+  },
+  {
+    "revision": "11d9822e1b9320e4baf4",
+    "url": "/css/chunk-vendors.ec8ed08e.css"
+  },
+  {
+    "revision": "8a4ba0ea9b1e5772d7163aba42bcc437",
+    "url": "/img/brand.8a4ba0ea.svg"
+  },
+  {
+    "revision": "784772692546022f648ac9b82702abee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b00f04b9b31b06ef272a",
+    "url": "/js/app-legacy.189c3445.js"
+  },
+  {
+    "revision": "11d9822e1b9320e4baf4",
+    "url": "/js/chunk-vendors-legacy.e348db00.js"
+  },
+  {
+    "revision": "6e072850c614ec08708283370da60f85",
+    "url": "/manifest.json"
+  }
+]);
